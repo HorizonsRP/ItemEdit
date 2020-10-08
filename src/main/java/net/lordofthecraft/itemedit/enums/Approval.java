126c0ff2bf36299db721150abd9a4a4e228c0b8c
Matthew Hildebrand <korvic@live.ca>
2020-10-08 10:20:04 -0500

    Just use Color.decode raw for IDE compatibility.

--- src/main/java/net/lordofthecraft/itemedit/enums/Approval.java
+++ src/main/java/net/lordofthecraft/itemedit/enums/Approval.java
@@ -6,6 +6,7 @@ import net.lordofthecraft.itemedit.ItemEdit;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +14,13 @@ public enum Approval {
 
 	// STAFF AND DEFAULT SIGNATURES
 	PLUGIN("PLUGIN", ChatColor.DARK_GRAY,                           "A Plugin",       ItemEdit.PERMISSION_START + ".plugin"),
-	ADMIN( "ADMIN",  ChatColor.of(ColorUtil.hexToColor("#1abc9c")), "A Team Lead",    ItemEdit.PERMISSION_START + ".admin"),
-	MOD(   "MOD",    ChatColor.of(ColorUtil.hexToColor("#1a67ed")), "A Moderator",    ItemEdit.PERMISSION_START + ".mod"),
-	TECH(  "TECH",   ChatColor.of(ColorUtil.hexToColor("#95de16")), "A Technician",   ItemEdit.PERMISSION_START + ".tech"),
-	LORE(  "LORE",   ChatColor.of(ColorUtil.hexToColor("#fae36e")), "A Lore Herald",  ItemEdit.PERMISSION_START + ".lore"),
-	EVENT( "EVENT",  ChatColor.of(ColorUtil.hexToColor("#cf0606")), "An Event Host",  ItemEdit.PERMISSION_START + ".event"),
-	BUILD( "BUILD",  ChatColor.of(ColorUtil.hexToColor("#ec9706")), "A Builder",      ItemEdit.PERMISSION_START + ".build"),
-	DESIGN("DESIGN", ChatColor.of(ColorUtil.hexToColor("#8634b3")), "A Designer",     ItemEdit.PERMISSION_START + ".design"),
+	ADMIN("ADMIN",   ChatColor.of(Color.decode("#1abc9c")), "A Team Lead", ItemEdit.PERMISSION_START + ".admin"),
+	MOD(   "MOD",    ChatColor.of(Color.decode("#1a67ed")), "A Moderator",    ItemEdit.PERMISSION_START + ".mod"),
+	TECH(  "TECH",   ChatColor.of(Color.decode("#95de16")), "A Technician",   ItemEdit.PERMISSION_START + ".tech"),
+	LORE(  "LORE",   ChatColor.of(Color.decode("#fae36e")), "A Lore Herald",  ItemEdit.PERMISSION_START + ".lore"),
+	EVENT( "EVENT",  ChatColor.of(Color.decode("#cf0606")), "An Event Host",  ItemEdit.PERMISSION_START + ".event"),
+	BUILD( "BUILD",  ChatColor.of(Color.decode("#ec9706")), "A Builder",      ItemEdit.PERMISSION_START + ".build"),
+	DESIGN("DESIGN", ChatColor.of(Color.decode("#8634b3")), "A Designer",     ItemEdit.PERMISSION_START + ".design"),
 	PLAYER("PLAYER", ChatColor.GRAY,                                "A Player",       ItemEdit.PERMISSION_START + ".use");
 
 	// Permissions note: Add .use to use, and .edit to be able to edit over.
